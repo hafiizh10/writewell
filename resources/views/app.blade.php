605ab14f85d8f1f0e9e93909f70e20a177db2e37
Zoelva <hafiizh10@gmail.com>
2023-09-09 22:01:20 +0800

    install inertia & react

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Write Well</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
